496f07155d7d74c5c7fa38c4a46415d66dbdfa22
AdarshGrewal <adarshgrewal@gmail.com>
2023-07-06 23:05:27 +0530

    DNM sm8450-common: Switch to permissive selinux
    Change-Id: I9d537406ab5601b9ba1e7bfc2cc65908cd16f8cc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,7 +104,8 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.selinux=permissive
 
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
